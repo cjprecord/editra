6c05dc52756eb49d7e6f5d5cdd59b0fc618213aa
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-03-25 09:10:28 +0000

    New Templates and Script updates

---
+++ trunk/scripts/i18n/gen_lang.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# generates po and mo files from the project source code
+
+ARG=$1
+
+print_help () {
+	echo
+	echo "Usage: $0 [-h|-mo|-po]"
+	echo "    -h    Print this help message"
+	echo "    -mo   Generate mo files and install them in the locale direcory"
+	echo "    -po   Generate new po files from the project source"
+	echo
+}
+
+if [ "$ARG" = "-po" ]
+then
+    env python2.5 mki18n.py -pv --domain=Editra
+elif [ "$ARG" = "-mo" ]
+then
+    env python2.5 mki18n.py -mv --domain=Editra --moTarget=../../locale
+else
+    print_help
+fi    
