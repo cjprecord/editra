2ebe1d26ac62c06fba36635a2e481c81ecd0d219
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-04-20 14:46:13 +0000

    Near complete implementation of the updater service. This included adding a separate dialog to process the download so that the prefdlg could be closed and make the editor usable during an update.

---
+++ trunk/src/ed_event.py
@@ -0,0 +1,66 @@
+############################################################################
+#    Copyright (C) 2007 Cody Precord                                       #
+#    cprecord@editra.org                                                   #
+#                                                                          #
+#    Editra is free software; you can redistribute it and#or modify        #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    Editra is distributed in the hope that it will be useful,             #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+"""
+#--------------------------------------------------------------------------#
+# FILE: ed_event.py
+# AUTHOR:
+# LANGUAGE: Python
+# SUMMARY:
+#    Provides custom events for the editors controls/objects to utilize
+#
+# METHODS:
+#
+#
+#
+#--------------------------------------------------------------------------#
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__cvsid__ = "$Id: Exp $"
+__revision__ = "$Revision:  $"
+
+#--------------------------------------------------------------------------#
+# Dependancies
+import wx
+import ed_glob
+
+#--------------------------------------------------------------------------#
+
+edEVT_UPDATE_TEXT = wx.NewEventType()
+EVT_UPDATE_TEXT = wx.PyEventBinder(edEVT_UPDATE_TEXT, 1)
+class UpdateTextEvent(wx.PyCommandEvent):
+    def __init__(self, evtType, id, value = None):
+        wx.PyCommandEvent.__init__(self, evtType, id)
+        self._evtType = evtType
+        self._id = id
+        self._value = value
+
+    def GetEvtType(self):
+        """Returns the event type"""
+        return self._evtType
+
+    def GetId(self):
+        """Returns the event id"""
+        return self._id
+
+    def GetValue(self):
+        """Returns the value from the event."""
+        return self_value
