3547ad22f84d0a79af12a3df264fabd9ddaf5fcb
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-05-05 11:11:23 +0000

    Updated the base profile information

--- profiles/default.pp
+++ profiles/default.pp
@@ -4,15 +4,27 @@
 # Lable			Value #
 #-----------------------------#
 AALIASING	True
+ALPHA           255
+AUTO_COMP       True
+AUTO_INDENT     True
 BRACKETHL	True
+CODE_FOLD       True
 DEFAULT		False
 EOL		Unix (\n)
 FHIST_LVL	5
 GUIDES		True
-ICONS		Stock
+ICONS		Nuovo
+ICON_SZ         (24,24)
 KWHELPER	True
-LANG		English
-MODE		DEBUG
+LANG		Default
+MODE		CODE
+MYPROFILE       default.pp
+PRINT_MODE      BLACK/WHITE
+SAVE_POS        True
+SET_WPOS        True
+SET_WSIZE       True
+SHOW_EOL        False
+SHOW_LN         True
 SHOW_WS		False
 SYNTAX		True
 TABWIDTH	8
@@ -20,6 +32,7 @@ THEME		DEFAULT
 TOOLBAR		True
 USETABS		True
 WRAP		True
+WSIZE           (700,450)
 
 
 EOF
