59eb74ac3781c2d5a4f7f557ac823bac01763e3b
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-05-13 22:32:51 +0000

    File cleanup and licensing notice added

--- trunk/src/plugin.py
+++ trunk/src/plugin.py
@@ -17,6 +17,37 @@
 #    Free Software Foundation, Inc.,                                       #
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
+#
+# Some of the code in this document is directly derived from trac
+#
+# Copyright (C) 2003-2006 Edgewall Software
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#  3. The name of the author may not be used to endorse or promote
+#     products derived from this software without specific prior
+#     written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
 #--------------------------------------------------------------------------#
@@ -113,12 +144,6 @@ class PluginMeta(type):
             for init in [b.__init__._original for b in new_obj.mro()
                          if issubclass(b, Plugin) and '__init__' in b.__dict__]:
                 break
-#         def maybe_init(self, pluginmgr, init=init, cls=new_obj):
-#             pluginmgr._plugins[cls] = self
-#             if init:
-#                 init(self, pluginmgr)
-#         maybe_init._original = init
-#         new_obj.__init__ = maybe_init
         PluginMeta._plugins.append(new_obj)
         for interface in d.get('_implements', []):
             PluginMeta._registry.setdefault(interface, []).append(new_obj)
