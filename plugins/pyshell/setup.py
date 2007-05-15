e2860d7834a6e73ef864442d908624b11bc54534
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-05-15 14:25:10 +0000

    Added profile support to the pyshell plugin and made a more complete setup file

--- plugins/pyshell/setup.py
+++ plugins/pyshell/setup.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-"""Setup script to build the PyShell plugin. To build the plugin
-just run setup.py bdist_egg and copy the egg to the plugin directory
+# Setup script to build the PyShell plugin. To build the plugin
+# just run 'python setup.py' and an egg will be built and put into 
+# the plugin directory
+"""Adds an interactive PyShell option to Editra's view menu. When
+active the PyShell will be placed in the bottom portion of Editra's
+frame, but it is dockable it can be moved to any other part of the frame
+or pulled out as a standalone dialog by use of the mouse. This plugin
+also interacts with Editra's PROFILE object so that it can remember if
+it was active or not in the users last session.
 
 """
+
 import sys
 try:
     from setuptools import setup
@@ -10,14 +18,20 @@ except ImportError:
     print "You must have setup tools installed in order to build this plugin"
     setup = None
 
-__author__ = "Cody Precord <cprecord@editra.org>"
+__author__ = "Cody Precord"
+
 if setup != None:
+    sys.argv.append("bdist_egg")
     sys.argv.append("--dist-dir=../.")
     setup(
         name='PyShell',
         version='0.1',
         description=__doc__,
         author=__author__,
+	author_email="cprecord@editra.org",
+	license="GPLv2",
+	url="editra.org",
+	platforms=["Linux", "OS X", "Windows"],
         packages=['pyshell'],
         entry_points='''
         [Editra.plugins]
