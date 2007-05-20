77372fd92c8196538d21c2bf6c03896f38a6c6dd
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-05-20 07:51:57 +0000

    Updated Language files

--- scripts/i18n/gen_lang.sh
+++ scripts/i18n/gen_lang.sh
@@ -7,7 +7,7 @@ print_help () {
 	echo
 	echo "Usage: $0 [-h|-mo|-po]"
 	echo "    -h    Print this help message"
-	echo "    -mo   Generate mo files and install them in the locale direcory"
+	echo "    -mo   Generate mo files and install them in the locale directory"
 	echo "    -po   Generate new po files from the project source"
 	echo
 }
