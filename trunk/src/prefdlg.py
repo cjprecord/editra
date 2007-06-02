1a63086741e15d5aa3595c865a942ff5f9342bc2
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-06-02 14:21:07 +0000

    Removed the plugin page from the prefdlg. The plugin manager no handles that pages tasks. With this checkin the plugin manager can fetch a list of available plugins from the website and download and install the selected ones. It's near complete just some rough edges to work out and a few more features to add and it will be in its final form for the next release.

--- trunk/src/prefdlg.py
+++ trunk/src/prefdlg.py
@@ -258,7 +258,6 @@ class PrefPages(wx.Notebook):
         self.MiscPage()
         self.CodePage()
         self.TextPage()
-        self.PluginPage()
         self.UpdatePage()
         self.ProfilePage()
 
@@ -472,56 +471,6 @@ class PrefPages(wx.Notebook):
         border.Add(misc_sizer, 0, wx.BOTTOM | wx.LEFT, 30)
         text_panel.SetSizer(border)
         self.AddPage(text_panel, _("Text"))
-
-    # XXX there are some display issues to work on with the list control
-    #     but everything is working for now.
-    def PluginPage(self):
-        """Plugin Configuration Page"""
-        plug_panel = wx.Panel(self, wx.ID_ANY)
-
-        # Items
-        plug_lbl = self.SectionHead(plug_panel, _("Plugin Settings"))
-        plug_lst = PluginListCtrl(plug_panel)
-        
-        p_mgr = wx.GetApp().GetPluginManager()
-        plug_lst.InsertColumn(0, _("Plugin"))
-        plug_lst.InsertColumn(1, _("Description"))
-        plug_lst.InsertColumn(2, _("Author"))
-        plug_lst.InsertColumn(3, _("Version"))
-        for item in p_mgr.GetConfig():
-            mod = sys.modules.get(item)
-            try:
-                doc = str(mod.__doc__)
-            except:
-                doc = _("No Description Available")
-            try:
-                auth = str(mod.__author__)
-            except:
-                auth = _("Unknown")
-            try:
-                ver = str(mod.__version__)
-            except:
-                ver = _("Unknown")
-
-            index = plug_lst.InsertStringItem(sys.maxint, item)
-            plug_lst.SetStringItem(index, 0, item)
-            plug_lst.CheckItem(index, p_mgr._config[item])
-            plug_lst.SetStringItem(index, 1, doc)
-            plug_lst.SetStringItem(index, 2, auth) #str(item.__module__.__author__))
-            plug_lst.SetStringItem(index, 3, ver) #str(item.__module__.__version__))
-
-        plug_lst.SetColumnWidth(0, wx.LIST_AUTOSIZE)
-        plug_lst.SetColumnWidth(1, wx.LIST_AUTOSIZE)
-        plug_lst.SetColumnWidth(2, wx.LIST_AUTOSIZE)
-        plug_lst.SetColumnWidth(3, wx.LIST_AUTOSIZE)
-
-        # Layout
-        border = wx.BoxSizer(wx.VERTICAL)
-        border.Add(wx.Size(15,15))
-        border.Add(plug_lbl, 0, wx.ALIGN_LEFT)
-        border.Add(plug_lst, 1, wx.EXPAND)
-        plug_panel.SetSizer(border)
-        self.AddPage(plug_panel, _("Plugins"))
 
     def MiscPage(self):
         """Misc preference page"""
