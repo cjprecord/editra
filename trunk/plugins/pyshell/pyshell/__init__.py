f10b1926a04403468887928971fba65293c4707f
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-06-07 13:15:10 +0000

    Some minor bug fixes and updates for the pyshell plugin

--- trunk/plugins/pyshell/pyshell/__init__.py
+++ trunk/plugins/pyshell/pyshell/__init__.py
@@ -18,10 +18,13 @@
 #    Free Software Foundation, Inc.,                                       #
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
+# Plugin Metadata
 """Adds a PyShell to the View Menu"""
 __author__ = "Cody Precord"
-__version__ = "0.1"
+__version__ = "0.2"
 
+#-----------------------------------------------------------------------------#
+# Imports
 import wx
 import wx.aui
 from wx.py import shell
@@ -30,7 +33,14 @@ import ed_main
 import ed_menu
 import plugin
 
+#-----------------------------------------------------------------------------#
+# Globals
+PANE_NAME = u'PyShell'
 ID_PYSHELL = wx.NewId()
+_ = wx.GetTranslation
+
+#-----------------------------------------------------------------------------#
+# Interface Implementation
 class PyShell(plugin.Plugin):
     """Adds a PyShell to the MainWindow's View Menu"""
     plugin.Implements(ed_main.MainWindowI)
@@ -42,20 +52,32 @@ class PyShell(plugin.Plugin):
             self._log("[pyshell] Installing PyShell Plugin")
             mb = mw.GetMenuBar()
             vm = mb.GetMenuByName("view")
-            pysh = vm.InsertAlpha(ID_PYSHELL, _("PyShell"), _("Show A Python Shell"), 
-            wx.ITEM_CHECK, after=ed_glob.ID_PRE_MARK)
-            pysh.Check(ed_glob.PROFILE.get('PYSHELL', False))
-            mw.Bind(wx.EVT_MENU, self.OnShowShell, id = ID_PYSHELL)
+            self._mi = vm.InsertAlpha(ID_PYSHELL, _(PANE_NAME), _("Show A Python Shell"), 
+                                      wx.ITEM_CHECK, after=ed_glob.ID_PRE_MARK)
+            self._mi.Check(ed_glob.PROFILE.get('PYSHELL', False))
             pyshell = shell.Shell(mw, wx.ID_ANY)
-            mw._mgr.AddPane(pyshell, wx.aui.AuiPaneInfo().Name("PyShell").\
+            mw._mgr.AddPane(pyshell, wx.aui.AuiPaneInfo().Name(PANE_NAME).\
                             Caption("Editra | PyShell").Bottom().Layer(0).\
                             CloseButton(True).MaximizeButton(False).\
                             BestSize(wx.Size(500,250)))
             if ed_glob.PROFILE.get('PYSHELL', False):
-                mw._mgr.GetPane("PyShell").Show()
+                mw._mgr.GetPane(PANE_NAME).Show()
             else:
                 ed_glob.PROFILE['PYSHELL'] = False
-                mw._mgr.GetPane("PyShell").Hide()
+                mw._mgr.GetPane(PANE_NAME).Hide()
+            
+            # Event Handlers
+            mw.Bind(wx.EVT_MENU, self.OnShowShell, id = ID_PYSHELL)
+            mw.Bind(wx.aui.EVT_AUI_PANE_CLOSE, self.OnPaneClose)
+
+    def OnPaneClose(self, evt):
+        """Handles when a pane is closed and updates the profile"""
+        pane = evt.GetPane()
+        if pane.name == PANE_NAME:
+            ed_glob.PROFILE['PYSHELL'] = False
+            self._mi.Check(False)
+        else:
+            evt.Skip()
 
     def OnShowShell(self, evt):
         """Shows the python shell frame"""
