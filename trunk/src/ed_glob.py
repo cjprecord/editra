5f46e16b067998109578a082b61029f0f02b95b2
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-06-15 12:56:36 +0000

    RTF file generator/exporter

--- trunk/src/ed_glob.py
+++ trunk/src/ed_glob.py
@@ -280,4 +280,4 @@ ID_2_PROF = {
 }
 
 # Default Plugins
-DEFAULT_PLUGINS = ("generator.Html", "generator.LaTeX") #, "generator.Rtf")
+DEFAULT_PLUGINS = ("generator.Html", "generator.LaTeX", "generator.Rtf")
