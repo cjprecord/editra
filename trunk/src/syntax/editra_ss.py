35c71e89e57de6b9c036afa15c50d975eef8db12
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-07-11 09:25:40 +0000

    some fixes/improvements to syntax files

--- trunk/src/syntax/editra_ss.py
+++ trunk/src/syntax/editra_ss.py
@@ -34,12 +34,13 @@ __svnid__ = "$Id$"
 __revision__ = "$Revision$"
 
 #-----------------------------------------------------------------------------#
+import synglob
+#-----------------------------------------------------------------------------#
 
 #---- Keyword Specifications ----#
 
 # Editra Style Sheet Keywords
 ESS_KEYWORDS = (0, "fore back face size eol bold italic")
-
 
 #---- Syntax Style Specs ----#
 SYNTAX_ITEMS = [ ('STC_CSS_DEFAULT', 'default_style'),
@@ -69,28 +70,40 @@ def Keywords(lang_id=0):
     @param lang_id: used to select specific subset of keywords
 
     """
-    return [ESS_KEYWORDS]
+    if lang_id == synglob.ID_LANG_ESS:
+        return [ESS_KEYWORDS]
+    else:
+        return list()
 
 def SyntaxSpec(lang_id=0):
     """Syntax Specifications
     @param lang_id: used for selecting a specific subset of syntax specs
 
     """
-    return SYNTAX_ITEMS
+    if lang_id == synglob.ID_LANG_ESS:
+        return SYNTAX_ITEMS
+    else:
+        return list()
 
 def Properties(lang_id=0):
     """Returns a list of Extra Properties to set
     @param lang_id: used to select a specific set of properties
 
     """
-    return [FOLD]
+    if lang_id == synglob.ID_LANG_ESS:
+        return [FOLD]
+    else:
+        return list()
 
 def CommentPattern(lang_id=0):
     """Returns a list of characters used to comment a block of code
     @param lang_id: used to select a specific subset of comment pattern(s)
 
     """
-    return list()
+    if lang_id == synglob.ID_LANG_ESS:
+        return list()
+    else:
+        return list()
 #---- End Required Functions ----#
 
 #---- Syntax Modules Internal Functions ----#
