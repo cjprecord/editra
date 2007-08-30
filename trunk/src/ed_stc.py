cffcfb8112d7dc89268fd5766ba301b1a78b2908
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-08-30 14:40:13 +0000

    Better handle errors when saving a file

--- trunk/src/ed_stc.py
+++ trunk/src/ed_stc.py
@@ -1838,10 +1838,11 @@ class EDSTC(wx.stc.StyledTextCtrl, ed_style.StyleMgr):
                 bom = u''
             writer.write(bom + self.GetText())
             writer.close()
-        except IOError:
+        except IOError, AttributeError, msg:
             writer.close()
             result = False
             self.LOG("[stc][err]There was an error saving %s" % path)
+            self.LOG("[stc][err] ERROR: %s" % str(msg))
         if result:
             self.SetSavePoint()
             self.modtime = util.GetFileModTime(path)
