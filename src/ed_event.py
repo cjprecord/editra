026e4f46d4d5b5b9efc28ce13d48281e7fea78aa
CodyPrecord <CodyPrecord@9353f5fb-1f26-0410-9bd0-5d4bb2fa6346>
2007-10-15 13:01:17 +0000

    Major code simplification and refactoring

--- src/ed_event.py
+++ src/ed_event.py
@@ -20,28 +20,24 @@
 
 """
 #--------------------------------------------------------------------------#
-# FILE: ed_event.py
-# @author: Cody Precord
-# LANGUAGE: Python
-# @summary:
-#    Provides custom events for the editors controls/objects to utilize
-#
-# METHODS:
-#
-#
-#
+# FILE: ed_event.py                                                        #
+# AUTHOR: Cody Precord                                                     #
+# LANGUAGE: Python                                                         #
+# SUMMARY:                                                                 #
+#  Provides custom events for the editors controls/objects to utilize      #
+#                                                                          #
 #--------------------------------------------------------------------------#
 """
 
 __author__ = "Cody Precord <cprecord@editra.org>"
-__cvsid__ = "$Id$"
+__svnid__ = "$Id$"
 __revision__ = "$Revision$"
 
-#--------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 # Dependancies
 import wx
 
-#--------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 
 edEVT_UPDATE_TEXT = wx.NewEventType()
 EVT_UPDATE_TEXT = wx.PyEventBinder(edEVT_UPDATE_TEXT, 1)
@@ -50,23 +46,7 @@ class UpdateTextEvent(wx.PyCommandEvent):
     def __init__(self, etype, eid, value=None):
         """Creates the event object"""
         wx.PyCommandEvent.__init__(self, etype, eid)
-        self._etype = etype
-        self._id = eid
         self._value = value
-
-    def GetEvtType(self):
-        """Returns the event type
-        @return: this events event type (ed_event)
-
-        """
-        return self._etype
-
-    def GetId(self):
-        """Returns the event id
-        @return: The Id of this event
-
-        """
-        return self._id
 
     def GetValue(self):
         """Returns the value from the event.
@@ -79,68 +59,19 @@ class UpdateTextEvent(wx.PyCommandEvent):
 
 edEVT_NOTIFY = wx.NewEventType()
 EVT_NOTIFY = wx.PyEventBinder(edEVT_NOTIFY, 1)
-class NotificationEvent(wx.PyCommandEvent):
+class NotificationEvent(UpdateTextEvent):
     """General notification event"""
     def __init__(self, etype, eid, value=None, obj=None):
-        wx.PyCommandEvent.__init__(self, etype, eid)
-        self._etype = etype
-        self._id = eid
-        self._value = value
-        self._obj = obj
-
-    def GetEventObject(self):
-        """Returns the object associated with this event
-        @return: the object associated with this event
-
-        """
-        return self._obj
-
-    def GetEvtType(self):
-        """Returns the event type
-        @return: this events event type (ed_event)
-
-        """
-        return self._etype
-
-    def GetId(self):
-        """Returns the event id
-        @return: the identifier of this event
-
-        """
-        return self._id
-
-    def GetValue(self):
-        """Returns the value from the event.
-        @return: the value of this event
-
-        """
-        return self._value
+        UpdateTextEvent.__init__(self, etype, eid, value)
+        self.SetEventObject(obj)
 
 #--------------------------------------------------------------------------#
 
 edEVT_MAINWINDOW_EXIT = wx.NewEventType()
 EVT_MAINWINDOW_EXIT = wx.PyEventBinder(edEVT_MAINWINDOW_EXIT, 1)
 class MainWindowExitEvent(wx.PyCommandEvent):
-    """Event to signal that text needs updating"""
-    def __init__(self, etype, eid):
-        """Creates the event object"""
-        wx.PyCommandEvent.__init__(self, etype, eid)
-        self._etype = etype
-        self._id = eid
-
-    def GetEvtType(self):
-        """Returns the event type
-        @return: this events event type (ed_event)
-
-        """
-        return self._etype
-
-    def GetId(self):
-        """Returns the event id
-        @return: the identifier of this event
-
-        """
-        return self._id
+    """Event to signal that the main window is exiting"""
+    pass
 
 #--------------------------------------------------------------------------#
 
@@ -149,7 +80,8 @@ EVT_STATUS = wx.PyEventBinder(edEVT_STATUS, 1)
 class StatusEvent(wx.PyCommandEvent):
     """Event for posting status events"""
     def __init__(self, etype, eid, msg=None, sec=0):
-        """Create the event
+        """Create an event that can be used to post status messages
+        to the main windows status bar.
         @param etype: The type of event to create
         @param eid: The event id
         @keyword msg: The status message to post with the event
@@ -157,24 +89,8 @@ class StatusEvent(wx.PyCommandEvent):
 
         """
         wx.PyCommandEvent.__init__(self, etype, eid)
-        self._etype = etype
-        self._id = eid
         self._msg = msg
         self._sec = sec
-
-    def GetEvtType(self):
-        """Returns the event type
-        @return: this events event type (ed_event)
-
-        """
-        return self._etype
-
-    def GetId(self):
-        """Returns the event id
-        @return: the identifier of this event
-
-        """
-        return self._id
 
     def GetMessage(self):
         """Returns the value from the event.
