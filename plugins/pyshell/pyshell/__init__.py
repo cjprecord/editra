b9d547d27a4a904f6dfa4b8066fa0cd23ba430e8
Cody Precord <cjprecord@beckman.com>
2019-11-15 09:42:01 -0600

    update for path name change

--- plugins/pyshell/pyshell/__init__.py
+++ plugins/pyshell/pyshell/__init__.py
@@ -1,38 +1,39 @@
 # -*- coding: utf-8 -*-
-############################################################################
-#    Copyright (C) 2007 Cody Precord                                       #
-#    cprecord@editra.org                                                   #
-#                                                                          #
-#    Editra is free software; you can redistribute it and#or modify        #
-#    it under the terms of the GNU General Public License as published by  #
-#    the Free Software Foundation; either version 2 of the License, or     #
-#    (at your option) any later version.                                   #
-#                                                                          #
-#    Editra is distributed in the hope that it will be useful,             #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#    GNU General Public License for more details.                          #
-#                                                                          #
-#    You should have received a copy of the GNU General Public License     #
-#    along with this program; if not, write to the                         #
-#    Free Software Foundation, Inc.,                                       #
-#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-############################################################################
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: PyShell Plugin                                                     #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2007 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
 # Plugin Metadata
 """Adds a PyShell to the Shelf"""
 __author__ = "Cody Precord"
-__version__ = "0.4"
+__version__ = "0.9"
 
 #-----------------------------------------------------------------------------#
 # Imports
+import os
 import wx
+import wx.stc
 from wx.py import shell
+
+# Local Imports
+import ed_glob
+import util
 import iface
-from profiler import Profile_Get
+import ed_style
 import plugin
+from profiler import Profile_Get, Profile_Set
+import syntax.syntax as syntax
+import eclib
+import ed_basewin
 
 #-----------------------------------------------------------------------------#
 # Globals
+
+PYSHELL_STYLE = "PyShell.Style" # Profile Key
+
 _ = wx.GetTranslation
 
 #-----------------------------------------------------------------------------#
@@ -40,38 +41,7 @@ _ = wx.GetTranslation
 class PyShell(plugin.Plugin):
     """Adds a PyShell to the Shelf"""
     plugin.Implements(iface.ShelfI)
-    ID_PYSHELL = wx.NewId()
     __name__ = u'PyShell'
-
-    def __SetupFonts(self):
-        """Create the font settings for the shell"""
-        fonts = { 
-                  'size'      : 11,
-                  'lnsize'    : 10,
-                  'backcol'   : '#FFFFFF',
-                  'calltipbg' : '#FFFFB8',
-                  'calltipfg' : '#404040',
-        }
-
-        font = Profile_Get('FONT1', 'font', wx.Font(11, wx.FONTFAMILY_MODERN, 
-                                                        wx.FONTSTYLE_NORMAL, 
-                                                        wx.FONTWEIGHT_NORMAL))
-        if font.IsOk() and len(font.GetFaceName()):
-            fonts['mono'] = font.GetFaceName()
-            fonts['size'] = font.GetPointSize()
-            if fonts['size'] < 11:
-                fonts['size'] = 11
-            fonts['lnsize'] = fonts['size'] - 1
-
-        font = Profile_Get('FONT2', 'font', wx.Font(11, wx.FONTFAMILY_SWISS, 
-                                                        wx.FONTSTYLE_NORMAL, 
-                                                        wx.FONTWEIGHT_NORMAL))
-        if font.IsOk() and len(font.GetFaceName()):
-            fonts['times'] = font.GetFaceName()
-            fonts['helv'] = font.GetFaceName()
-            fonts['other'] = font.GetFaceName()
-
-        return fonts
 
     def AllowMultiple(self):
         """PyShell allows multiple instances"""
@@ -79,18 +49,122 @@ class PyShell(plugin.Plugin):
 
     def CreateItem(self, parent):
         """Returns a PyShell Panel"""
-        self._log = wx.GetApp().GetLog()
-        self._log("[pyshell][info] Creating PyShell instance for Shelf")
-        pyshell = shell.Shell(parent, locals=dict())
-        pyshell.setStyles(self.__SetupFonts())
-        return pyshell
+        util.Log("[PyShell][info] Creating PyShell instance for Shelf")
+        return EdPyShellBox(parent)
+
+    def GetBitmap(self):
+        """Get the bitmap for representing this control in the ui
+        @return: wx.Bitmap
+
+        """
+        return wx.ArtProvider.GetBitmap(str(ed_glob.ID_PYSHELL), wx.ART_MENU)
 
     def GetId(self):
-        return self.ID_PYSHELL
+        return ed_glob.ID_PYSHELL
 
     def GetMenuEntry(self, menu):
-        return wx.MenuItem(menu, self.ID_PYSHELL, self.__name__, 
-                                        _("Open A Python Shell"))
+        return wx.MenuItem(menu, ed_glob.ID_PYSHELL, self.__name__, 
+                           _("Open A Python Shell"))
 
     def GetName(self):
         return self.__name__
+
+    def IsStockable(self):
+        return True
+
+#-----------------------------------------------------------------------------#
+
+class EdPyShellBox(ed_basewin.EdBaseCtrlBox):
+    """Control box for PyShell"""
+    def __init__(self, parent):
+        super(EdPyShellBox, self).__init__(parent)
+
+        # Attributes
+        self._shell = EdPyShell(self)
+        self._styles = util.GetResourceFiles(u'styles', True, True, title=False)
+        self._choice = None
+        self._clear = None
+
+        # Setup
+        self.__DoLayout()
+        style = self._shell.ShellTheme
+        for sty in self._styles:
+            if sty.lower() == style.lower():
+                self._choice.SetStringSelection(sty)
+                break
+
+        # Event handlers
+        self.Bind(wx.EVT_BUTTON, self.OnButton, self._clear)
+        self.Bind(wx.EVT_CHOICE, self.OnChoice, self._choice)
+
+    def __DoLayout(self):
+        """Layout the control box"""
+        ctrlbar = self.CreateControlBar()
+        if wx.Platform == '__WXGTK__':
+            ctrlbar.SetWindowStyle(eclib.CTRLBAR_STYLE_BORDER_BOTTOM)
+
+        self._choice = wx.Choice(ctrlbar, wx.ID_ANY, choices=self._styles)
+
+        ctrlbar.AddControl(wx.StaticText(ctrlbar, label=_("Color Scheme:")),
+                           wx.ALIGN_LEFT)
+        ctrlbar.AddControl(self._choice, wx.ALIGN_LEFT)
+        ctrlbar.AddStretchSpacer()
+        self._clear = self.AddPlateButton(_("Clear"), ed_glob.ID_DELETE,
+                                          wx.ALIGN_RIGHT)
+        self.SetWindow(self._shell)
+
+    def OnButton(self, evt):
+        """Button event handler
+        @param evt: wx.EVT_BUTTON
+
+        """
+        if evt.GetEventObject() == self._clear:
+            self._shell.clear()
+            self._shell.prompt()
+        else:
+            evt.Skip()
+
+    def OnChoice(self, evt):
+        """Change the color style
+        @param evt: wx.EVT_CHOICE
+
+        """
+        e_obj = evt.GetEventObject()
+        self._shell.ShellTheme = e_obj.GetStringSelection()
+
+#-----------------------------------------------------------------------------#
+
+class EdPyShell(shell.Shell, ed_style.StyleMgr):
+    """Custom PyShell that uses Editras StyleManager"""
+    def __init__(self, parent):
+        """Initialize the Shell"""
+        shell.Shell.__init__(self, parent, locals=dict())
+
+        # Get the color scheme to use
+        style = Profile_Get(PYSHELL_STYLE)
+        if style is None:
+            style = Profile_Get('SYNTHEME')
+
+        ed_style.StyleMgr.__init__(self, self.GetStyleSheet(style))
+
+        # Attributes
+        self.SetStyleBits(5)
+        self._shell_style = style
+        mgr = syntax.SyntaxMgr(ed_glob.CONFIG['CACHE_DIR'])
+        syn_data = mgr.GetSyntaxData('py')
+        synspec = syn_data.SyntaxSpec
+        self.SetLexer(wx.stc.STC_LEX_PYTHON)
+        self.SetSyntax(synspec)
+
+    def __SetShellTheme(self, style):
+        """Set the color scheme used by the shell
+        @param style: style sheet name (string)
+
+        """
+        self._shell_style = style
+        Profile_Set(PYSHELL_STYLE, style)
+        self.UpdateAllStyles(style)
+
+    ShellTheme = property(lambda self: self._shell_style,
+                          lambda self, style: self.__SetShellTheme(style))
+
